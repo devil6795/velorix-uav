05e336e6b8dbc0d218d344f52a41258fe72cb039
Velorix Builder <velorix@example.com>
2026-09-22 13:16:45 +0530

    Fix flex layout to prevent overlap on small height viewports

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -6,15 +6,23 @@ import { HeroMeta } from './HeroMeta';
 
 export function Hero() {
   return (
-    <section className="relative w-full h-screen min-h-[700px] md:min-h-[800px] flex flex-col justify-end pb-24 md:pb-32 px-6 md:px-12 lg:px-20 overflow-hidden">
+    <section className="relative w-full min-h-screen md:min-h-[800px] flex flex-col pt-[96px] overflow-hidden">
       {/* Media Background Layer (z-0) */}
       <HeroMedia />
 
       {/* Content Layer (z-10) */}
-      <HeroHeadline />
+      <div className="relative z-10 flex flex-col justify-between flex-1 w-full px-6 md:px-12 lg:px-20 pb-8 md:pb-12">
+        
+        {/* Centered Headline Container */}
+        <div className="flex-1 flex flex-col justify-center w-full py-8 min-h-[450px]">
+          <HeroHeadline />
+        </div>
 
-      {/* Meta Elements (z-10) */}
-      <HeroMeta />
+        {/* Meta Elements (z-10) */}
+        <div className="flex-shrink-0">
+          <HeroMeta />
+        </div>
+      </div>
 
       {/* Bottom fade into next section */}
       <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-background to-transparent z-10 pointer-events-none" />
